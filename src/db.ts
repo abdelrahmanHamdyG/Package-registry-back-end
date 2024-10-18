313f5655d5bbf13ee0d3bd515ea49a163f153f35
Zeyad7007 <zeyadel3fifi@gmail.com>
2024-10-18 16:45:54 -0400

    adding db.ts

---
+++ src/db.ts
@@ -0,0 +1,33 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: 'localhost',
+  database: 'PackageRegDB',
+  user: 'postgres',
+  password: 'datahead',
+  port: 5433,
+  // ssl options if needed
+});
+
+// Function to perform database operations
+async function initDB() {
+  try {
+    // Execute a query
+    const res = await pool.query('SELECT * FROM package');
+    console.log(res.rows);
+  } catch (err) {
+    console.error('Error in fetching data', err);
+  } finally {
+    // Close the pool when you're done
+    await pool.end();
+    console.log('Database connection closed.');
+  }
+}
+
+// Invoke the function
+initDB().then(() => {
+  console.log('Program completed.');
+}).catch(err => {
+  console.error('Unhandled error:', err);
+});
